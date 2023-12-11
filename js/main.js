3fcf7cc29a807026ed3e8c1b002ede4dd0b13b1b
AbdoSabry2003 <94800956+AbdoSabry2003@users.noreply.github.com>
2023-12-11 18:39:22 +0200

    localstorage

--- js/main.js
+++ js/main.js
@@ -33,25 +33,63 @@ const container = document.querySelector(".container"),
         container.classList.remove("active");
     });
 
-    //   js code to save user data in cookies
-    function storeInputs() {
-        var name = document.getElementById('name').value;
-        var email = document.getElementById('email').value;
+    let signUpButton = document.getElementById('signUp');
+let signInButton = document.getElementById('signIn');
+
+signUpButton.addEventListener("click", ( )=>{
+    container.classList.add("active");
+});
+signInButton.addEventListener("click", ( )=>{
+    container.classList.remove("active");
+});
+
+function storeInputs() {
+    // get user input
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    var confirmPassword = document.getElementById('confirmPassword').value;
+
+    if (password !== confirmPassword) {
+        alert('Passwords do not match.');
+        return;
+    }
+
+    var d = new Date();
+    d.setTime(d.getTime() + (30*24*60*60*1000)); // Expires in 30 days
+    var expires = "expires="+ d.toUTCString();
+
+    // store user data in local storage
+    localStorage.setItem("name", name);
+    localStorage.setItem("email", email);
+    localStorage.setItem("password", password);
+    localStorage.setItem("expires", expires);
+
+    // display successful message
+    alert('Account created successfully. You can now log in.');
+}
+
+function getStoredInputs() {
+    // get user data from local storage
+    var storedName = localStorage.getItem("name");
+    var storedEmail = localStorage.getItem("email");
+    var storedPassword = localStorage.getItem("password");
+    var storedExpires = localStorage.getItem("expires");
+
+    // check if user data exists in local storage
+    if (storedName && storedEmail && storedPassword && storedExpires) {
+        var name = document.getElementById('email').value;
         var password = document.getElementById('password').value;
-        var confirmPassword = document.getElementById('confirmPassword').value;
-        var isSeller = document.getElementById('termCon').checked;
 
-        if (password !== confirmPassword) {
-      alert('Passwords do not match.');
-      return;
+        if (name === storedEmail && password === storedPassword) {
+            // display successful message
+            alert('Welcome back, ' + storedName + '!');
+            // redirect to index.html page
+            window.location.href = "index.html";
+        } else {
+            alert('Incorrect email or password. Please try again.');
+        }
+    } else {
+        alert('You need to sign up before you can log in.');
     }
-
-        var d = new Date();
-        d.setTime(d.getTime() + (30*24*60*60*1000)); // Expires in 30 days
-        var expires = "expires="+ d.toUTCString();
-
-        document.cookie = "name=" + name + ";" + expires + ";path=/";
-        document.cookie = "email=" + email + ";" + expires + ";path=/";
-        document.cookie = "password=" + password + ";" + expires + ";path=/";
-        document.cookie = "isSeller=" + isSeller + ";" + expires + ";path=/";
-    }
+}
